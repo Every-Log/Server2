55d2baf55a6db67dba9f6cece63f2654f3b98af6
haeji <haejipark88@gmail.com>
2022-09-22 19:01:49 +0900

    환경설정

---
+++ everylog/src/test/java/tteokbokki/everylog/EverylogApplicationTests.java
@@ -0,0 +1,13 @@
+package tteokbokki.everylog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverylogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
