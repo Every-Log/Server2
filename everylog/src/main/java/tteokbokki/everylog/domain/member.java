e634fb494b15e59011e1f2cc37f629c85368fb02
haeji <haejipark88@gmail.com>
2022-09-25 02:52:24 +0900

    mysql datagrip db연동 완료 확인 및 gradle 설정추가

---
+++ everylog/src/main/java/tteokbokki/everylog/domain/member.java
@@ -0,0 +1,29 @@
+package tteokbokki.everylog.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Table(name = "MEMBER")
+@Entity
+@Getter
+@Setter
+public class member {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "member_id")
+        private Long number; //회원번호
+
+        @Column(nullable = false, unique = true)
+        private String id; //아이디
+
+        @Column
+        private String name; //닉네임
+
+        @Column
+        private String password; //비밀번호
+
+        @Column
+        private String image; //이미지 주소
+}
