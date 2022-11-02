b06463b7bb5ba970f7c80d5e79672fa9b6f7b299
choiminsun <backwonand@sookmyung.ac.kr>
2022-11-03 00:19:16 +0900

    Add findById Test code

--- everylog/src/test/java/tteokbokki/everylog/UserTest.java
+++ everylog/src/test/java/tteokbokki/everylog/UserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import tteokbokki.everylog.domain.User;
 import tteokbokki.everylog.dto.UserDto;
 import tteokbokki.everylog.service.UserService;
 
@@ -24,4 +25,15 @@ public class UserTest {
     }
 
     @Test
+    public void 회원조회(){
+        //이런 정보로 가입했을 때
+        User user = new User("1", "name", "pass", "img");
+        Long uId = userService.save(new UserDto(user));
+
+        //아이디를 넣고 조회하면
+        assertEquals("name", userService.findById(uId).getName(), "그 이름이 아닙니다만..");
+        assertEquals("pass", userService.findById(uId).getPassword(), "비밀번호가 달라요");
+        assertEquals("img", userService.findById(uId).getImage(), "이미지가 달라요");
+
+    }
 }
